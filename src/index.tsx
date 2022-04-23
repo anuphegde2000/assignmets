dd2d51864825bc7a559400e57d5a3bdea0e0cb08
Anup Hegde <anuphegde0202@gmail.com>
2022-04-23 16:46:34 +0530

    session 9 commit

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = "krishna";
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <App firstName={name} age={16} />
   </React.StrictMode>
 );
 
